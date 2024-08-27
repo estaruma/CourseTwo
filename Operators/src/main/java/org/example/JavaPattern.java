5e9e510583e940a358de165ed33a23f2052f7456
estaruma <esther.arumainayagam@mettle.co.uk>
2024-08-27 16:07:09 +0100

    Adding operators exercise

---
+++ Operators/src/main/java/org/example/JavaPattern.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class JavaPattern {
+    public static void main(String[] args) {
+        System.out.println("    J    " + "    A    " + "    V   " + "     V   " + "    A   ");
+        System.out.println("    J    " + "   A A   " + "    V V  " + "    V V  " + "   A A  ");
+        System.out.println("J   J    " + "  AAAAA  " + "    V V  " + "   V   V " + "  AAAAA ");
+        System.out.println(" J J     " + " A     A " + "     V   " + "   V   V " + " A     A");
+    }
+}
