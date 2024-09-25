84e9f7459ea80f6f2e459a9347c330e015f1ae99
estaruma <esther.arumainayagam@mettle.co.uk>
2024-09-25 16:27:45 +0100

    Add vowels exception exercise

--- VowelException/src/main/java/org/example/Main.java
+++ VowelException/src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
+// Write a Java program to create a method that takes a string as input and throws an exception if the string does not contain vowels.
+
 package org.example;
 
 import java.util.Scanner;
+import org.example.NoVowelException;
+import org.example.VowelChecker;
 
 public class Main {
     public static void main(String[] args) {
